b90e629aedcb205f80e1e94e691275ca07b5e90f
alex <khadasevich.aleksey@gmail.com>
2020-04-02 21:58:10 +0300

    Automation Engineer class

--- src/main/java/Day_3_Hometask_2/AutomationEngineer.java
+++ src/main/java/Day_3_Hometask_2/AutomationEngineer.java
@@ -1,7 +1,7 @@
 package Day_3_Hometask_2;
 
 public class AutomationEngineer {
-    public void executeTests () {
+    public void executeTests() {
         System.out.println("I can execute tests");
     }
 }
