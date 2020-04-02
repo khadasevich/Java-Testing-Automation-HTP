cd410df40fa50296a613dab2725804baa9cd3284
alex <khadasevich.aleksey@gmail.com>
2020-04-02 21:54:51 +0300

    Human Class

---
+++ src/main/java/Day_3_Hometask_2/Human.java
@@ -0,0 +1,15 @@
+package Day_3_Hometask_2;
+
+public class Human {
+
+    private double age;
+
+    public void setAge(double age)
+    {
+        this.age = age;
+    }
+
+    public void voice() {
+        System.out.println("I can speak. My age is " + age);
+    }
+}
