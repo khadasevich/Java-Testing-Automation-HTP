814071c02e329d3c5b57ea1773dd3085d7f8f701
Alex Khadasevich <khadasevich.aleksey@gmail.com>
2020-04-19 15:06:40 +0300

    Day 4 Updates

---
+++ src/main/java/day6class/Day6.java
@@ -0,0 +1,32 @@
+package day6class;
+
+public class Day6 {
+
+// Databases
+//
+// строка - запись кортеж, столбец - аттрибут, поле, таблица - отношение
+// добавление вторичных ключей - миграция первичных ключей
+// First normal form -
+//
+//
+// Second normal form -
+//
+//
+// Third normal form -
+//
+//
+//
+// нерезидентные базы данных -
+//
+// RLIKE allows to search data via regular expression
+// UNION requires same quantity of columns
+// UNION ALL to show duplicates in result as well
+// aggregate function works with all column
+// scalar function works with value, e.g. CHAR_LENGTH, CONCAT
+//
+
+
+    public static void main (String[] args){
+        System.out.println("Day 6");
+    }
+}
