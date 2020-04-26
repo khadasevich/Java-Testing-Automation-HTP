eabd5f3104920964672ba384983982bb1bf4012c
Alex Khadasevich <khadasevich.aleksey@gmail.com>
2020-04-26 15:33:03 +0300

    Day 9

---
+++ src/main/java/day9class/Day9.java
@@ -0,0 +1,33 @@
+package day9class;
+
+public class Day9 {
+
+//  XML, CSS, HTML
+//  Document Object Model
+//  TLS (SSL)
+// port: 443 - https
+// port: 80 - http
+// port: 22 - ssh
+// port: 3306 - MySql
+//
+// HTML title is required by RFC but cannot be in and browser can read it
+// quantity of ports - int
+//
+// ancestors rewrites styles of parent, computed shows real one
+//
+// Xpath can search siblings /following-sibling::input
+//
+//
+//
+//
+//
+//
+//
+
+
+    public static void main (String[] args){
+        System.out.println("Day 9");
+    }
+}
+
+
